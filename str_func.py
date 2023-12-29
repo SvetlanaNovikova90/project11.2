66b1ae9d8fd2e3bbca0626dd508f529b4e589db1
Svetlana Novikova <seaciliya2012@yandex.ru>
2023-12-29 19:44:22 +0300

    add docstring

--- str_func.py
+++ str_func.py
@@ -1,2 +1,15 @@
 def word_in_capital_letters(word):
+    '''
+    функция делает все буквы заглавными
+    :param word: str
+    :return: str
+    '''
     return word.upper()
+
+def first_letters_capitalized(text):
+    '''
+    функция делает первую букву каждого слова заглавной
+    :param text:
+    :return:
+    '''
+    return text.title()
